030320d57feb0ef82debb387698f9cb7d263a459
macabacus-ra <114944299+macabacus-ra@users.noreply.github.com>
2023-03-24 15:58:08 -0700

    Update stack.js

--- stack.js
+++ stack.js
@@ -82,8 +82,7 @@ function stackLeft(gapValue, unitValue) {
 }
 
 //============================================================================================================================
-//============================================================================================================================
-//============================================================================================================================
+
 
 function stackRight(gapValue, unitValue) {
   let gap = gapValue;
@@ -91,58 +90,26 @@ function stackRight(gapValue, unitValue) {
   let selections = SlidesApp.getActivePresentation().getSelection()
   let pageElementRange = selections.getPageElementRange()
   if (pageElementRange){
-
     let elements = pageElementRange.getPageElements()
 
     if(elements.length >= 2){  
-
       let nextShapePosition = 0;
-
-      // sort arrays from left position to determine order for the iterative operation of setting gap
-      let sorted = elements.sort((a, b) => b.getLeft() + b.getWidth() - (a.getLeft() + a.getWidth()));
-
-      //for the sort above, since we do not have a shape.right property
-      //a narrower width shape and a wider shape will not give the proper
-      // "right-most" shape because we are only accessing its left position.
-      // hence, we need to add it's width to ensure the right-most
-
-      // for the first element in the sorted shapes array
-      // we will not change its horizontal position
-      // for the next elements we will change their left position accordingly
-
-      let obj = {
-        firstLeftPosition: sorted[0].left,
-        nextLeft: null
-      };
+      let sorted = elements.sort((a, b) => (b.getLeft() + b.getWidth()) - (a.getLeft() + a.getWidth()));
 
       for (let i = 0; i < sorted.length; i++) {
-        if (i < 1) {
-          // first, right-most shape position will not change, just being specific here
-          sorted[i].left = obj.firstLeftPosition;
-          //for the next iteration, set the position of the next right-most shape
+        let rightPosition = nextShapePosition > 0 ? nextShapePosition : (sorted[i].getLeft() + sorted[i].getWidth())
 
-          obj.nextLeft = sorted[i].getLeft() - gap - sorted[i + 1].getWidth();
-
-        } else if (i === sorted.length - 1) {
-          //for the last iteration, we are not incrementing,this was causing the whole code to fail.
-          sorted[i].setLeft(obj.nextLeft)
-          
-        } else {
-          //everything in between the first and last shapes  happens here...
-
-          sorted[i].setLeft(obj.nextLeft);
-          obj.nextLeft = sorted[i].getLeft() - gap - sorted[i + 1].getWidth();
-
-        }
+        sorted[i].setLeft(rightPosition - sorted[i].getWidth());
+        nextShapePosition = sorted[i].getLeft() - gap;
       }
+
     }else{
       SlidesApp.getUi().alert('Select two or more shapes to perform this operation');
     }
   }       
 }
 //============================================================================================================================
-//============================================================================================================================
-//============================================================================================================================
+
 
 function stackUp(gapValue, unitValue) {
   let gap = gapValue;
@@ -168,6 +135,39 @@ function stackUp(gapValue, unitValue) {
 
         sorted[i].setTop(topPosition);
         nextShapePosition = sorted[i].getTop() + sorted[i].getHeight() + gap;
+      }
+    }else{
+      SlidesApp.getUi().alert('Select two or more shapes to perform this operation');
+    }
+  }       
+}
+
+//============================================================================================================================
+
+function stackDown(gapValue, unitValue) {
+  let gap = gapValue;
+  console.log(unitValue)
+
+  let selections = SlidesApp.getActivePresentation().getSelection()
+  let pageElementRange = selections.getPageElementRange()
+  if(!pageElementRange){ 
+    SlidesApp.getUi().alert('Select select two or more shapes to perform this operation');
+    return 
+  }
+  if (pageElementRange){
+    let elements = pageElementRange.getPageElements()
+
+    if(elements.length >= 2){  
+      let nextShapePosition = 0;
+
+      // sort arrays from left position to determine order for the iterative operation of setting gap
+      let sorted = elements.sort((a, b) => (b.getTop()+b.getHeight()) - (a.getTop()+a.getHeight()));
+
+      for (let i = 0; i < sorted.length; i++) {
+        let bottomPosition = nextShapePosition > 0 ? nextShapePosition : (sorted[i].getTop() + sorted[i].getHeight())
+
+        sorted[i].setTop(bottomPosition - sorted[i].getHeight());
+        nextShapePosition = sorted[i].getTop() - gap;
       }
     }else{
       SlidesApp.getUi().alert('Select two or more shapes to perform this operation');
