5d4fedfd6e89d2bbb7276483e9360fa96a60ca8e
macabacus-ra <114944299+macabacus-ra@users.noreply.github.com>
2023-08-08 16:06:20 -0700

    Update airplaneModeTesting.js

--- airplaneModeTesting.js
+++ airplaneModeTesting.js
@@ -1,131 +1,84 @@
 function turnOnAirplaneMode(){ 
   const userData = PropertiesService.getUserProperties();  
   let userDataVal = userData.getProperty('redactedImagesArray')
-
   if(!userDataVal){ userData.setProperty('redactedImagesArray', '') }
-
   let selectedImage = SlidesApp.getActivePresentation().getSelection().getPageElementRange().getPageElements()[0] 
 
   if(selectedImage.getPageElementType() == "IMAGE"){
- 
     let blob = selectedImage.asImage().getBlob() 
     // create a folder first, called macabacus?
-
-    let folder = DriveApp.getFoldersByName('macabacusX')
+    let folder = DriveApp.getFoldersByName('macabacus1')
 
     if(folder.hasNext()){
-
       // save image in folder then replace with black image
       let macabacusFolder = folder.next()
-
       let originalImageToRedact = macabacusFolder.createFile(blob)
 
-      let imageData = {
-        id: originalImageToRedact.getId(),
-        url: originalImageToRedact.getDownloadUrl()
+      // to drive
+      let imageData = { 
+        driveId: originalImageToRedact.getId(),
+        slidesId: selectedImage.getObjectId()
       }
 
       // get current list of redacted images 
-      let redacted = userData.getProperty('redactedImagesArray')
+      let redacted = userData.getProperty('redactedImagesArray1')
 
       if(!redacted){
-        userData.setProperty('redactedImagesArray', JSON.stringify([imageData]))
+        userData.setProperty('redactedImagesArray1', JSON.stringify([imageData]))
       }else{ 
         let array = JSON.parse(redacted)
         array.push(imageData)
-        userData.setProperty('redactedImagesArray', JSON.stringify(array))
+        userData.setProperty('redactedImagesArray1', JSON.stringify(array))
       }
 
       // we can now replace the image with the image from the drive 
       let blackImageFromDrive = userData.getProperty('redactImage')
-
-
-      let blackImageToInsert = JSON.parse(blackImageFromDrive)
-      // replace image, should also check to make sure this image exists
-
-      selectedImage.asImage().replace(blackImageToInsert.url, true)
-
-
-
-
-
-
-    //=============================================
-    // Error here, since URL cannot be access, we can not replace the image with the black image.
-    // so, here we need to insert new image, and remove the original
-
-
-
-    // we also need to keep track of all REDACTED or "black images" since we will be replacing each one!
-
-
-
-
-
-
-
-
-
-
-
+      let blackImageID = JSON.parse(blackImageFromDrive).id
+      let blob1 = DriveApp.getFileById(blackImageID).getBlob()
+      selectedImage.asImage().replace(blob1, true)
 
 
     }else{
-      let newFolder = DriveApp.createFolder('macabacusX')
+      let newFolder = DriveApp.createFolder('macabacus1')
 
       let originalImageToRedact = newFolder.createFile(blob)
 
       // add the original image
       let imageData = {
-        id: originalImageToRedact.getId(),
-        url: originalImageToRedact.getDownloadUrl()
+        driveId: originalImageToRedact.getId(),
+        slidesId: selectedImage.getObjectId()
       }
 
       // get current list of redacted images 
-      let redacted = userData.getProperty('redactedImagesArray')
+      let redacted = userData.getProperty('redactedImagesArray1')
 
       if(!redacted){
-        userData.setProperty('redactedImagesArray', JSON.stringify([imageData]))
+        userData.setProperty('redactedImagesArray1', JSON.stringify([imageData]))
       }else{ 
         let array = JSON.parse(redacted)
         array.push(imageData)
-        userData.setProperty('redactedImagesArray', JSON.stringify(array))
+        userData.setProperty('redactedImagesArray1', JSON.stringify(array))
       }
 
-
       //add the black image for redaction
-      
       let blackImage = selectedImage.asImage().replace('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Very_Black_screen.jpg/800px-Very_Black_screen.jpg?20200816082819', true) 
-
       let blackBlob = blackImage.getBlob()
-      
       let driveBlackImage = newFolder.createFile(blackBlob)
-
       userData.setProperty('blackImage', JSON.stringify({
-        id: driveBlackImage.getId(),
-        url: driveBlackImage.getDownloadUrl()
+        id: driveBlackImage.getId()
       }))
     }
   } 
 }
 
 function turnOffAirplaneMode(){
-
-//1w4gsJSSpSJMAZdxTeQcakgHxqa-mhYUI
   const userData = PropertiesService.getUserProperties();  
-  let userDataVal = userData.getProperty('airplaneMode')
-
+  let userDataVal = userData.getProperty('redactedImagesArray1')
   let data = JSON.parse(userDataVal)
-
-  let imageId = data[0].driveId
-
-  // console.log(imageId)
-
-  let imageFromDrive = DriveApp.getFileById(imageId)
-
-  // let imageToConvert = SlidesApp.getActivePresentation().getPageElementById(imageId)
-
-  console.log(imageFromDrive.getUrl())
-
-
+  let slidesId = data[0].slidesId
+  let driveFileId = data[0].driveId
+  // console.log(JSON.stringify(data))
+  let currentBlackImage = SlidesApp.getActivePresentation().getPageElementById(slidesId).asImage()
+  let originalDriveImageBlob = DriveApp.getFileById(driveFileId).getBlob()
+  currentBlackImage.replace(originalDriveImageBlob, true) 
 }
